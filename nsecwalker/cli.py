8583d9d967e7eb47d1bd254033ab572f549ebb99
Kyle Laker <kyle@laker.email>
2022-02-21 17:51:05 -0500

    Support retries

--- nsecwalker/cli.py
+++ nsecwalker/cli.py
@@ -7,6 +7,8 @@ import dns.name
 
 from nsecwalker import error, query
 
+ROOT_NAME = dns.name.from_text(".")
+
 _FORMATTERS = {}
 
 
@@ -28,7 +30,7 @@ def json_formatter(names):
     return json.dumps(names, indent=2, default=str)
 
 
-def walk(name):
+def walk(name: str):
     dns_name = dns.name.from_unicode(name)
     names = [dns_name]
     nameset = {dns_name}
@@ -43,6 +45,15 @@ def walk(name):
             # lookup based on the name
             names.append(result)
             nameset.add(result)
+            # There's not a ton of value in starting to chase NSEC records
+            # across the entire DNS space (though there is an NSEC at .).
+            # We should consider it the end of the zone. But by adding it
+            # to the list of names first, we avoid returning a message that
+            # there weren't any NSEC records (because there was one). Usually
+            # this is just an intentional mitigation against walking the zone.
+            if result == ROOT_NAME:
+                break
+
         # Attempt to preserve whatever records were able to be found
         # by breaking from the loop on a resolution error or Ctrl-C;
         # hopefully the write happens quick enough that it's not an
@@ -66,8 +77,18 @@ def walk(name):
 )
 def main(name, output):
     names = walk(name)
+
+    # Exit with a non-zero status code if there's only one name (or fewer)
+    # in the resulting list. This indicates that there was not an NSEC record
+    # present and that there was nothing to find. The `names` list will contain
+    # the name that was given as input (though likely canonicalized)
+    if len(names) <= 1:
+        click.echo(f"No NSEC records found for {name}", file=sys.stderr)
+        # This is the best way to set the return code because click doesn't
+        # care about the function's return value
+        sys.exit(1)
+
     click.echo(_FORMATTERS[output](names))
-    return 0
 
 
 if __name__ == "__main__":
--- nsecwalker/query.py
+++ nsecwalker/query.py
@@ -1,16 +1,39 @@
+import socket
+
 import dns.exception
 import dns.resolver
 import dns.name
+import dns.rdataclass
 import dns.rdatatype
 import dns.rrset
+import dns.query
+import dns.message
 
 from nsecwalker import error
 
+def do_lookup(name, max_tries=5):
+    # resolver = dns.resolver.Resolver()
+    dns.resolver.get_default_resolver().nameservers = ['8.8.8.8', '8.8.4.4']
+    # resolver.nameservers = ['8.8.8.8', '8.8.4.4']
+    for i in range (1, max(1, max_tries + 1)):
+        try:
+            return dns.resolver.resolve(name, rdtype=dns.rdatatype.NSEC)
+        except (
+            dns.exception.Timeout,
+            dns.resolver.NXDOMAIN,
+            dns.resolver.YXDOMAIN,
+            dns.resolver.NoAnswer,
+            dns.resolver.NoNameservers,
+        ) as e:
+            if i == max_tries:
+                raise e
 
 def query(name: str) -> dns.resolver.Answer:
     try:
-        result = dns.resolver.resolve(name, dns.rdatatype.NSEC)
+        result = do_lookup(name)
         names = [rdata.next for rdata in result]
+        if len(names) == 0:
+            return None
         if len(names) != 1:
             raise error.DuplicateNsecError(name)
         return names[0]
